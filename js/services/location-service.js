93f23350f8fb077e3607b58f0fe24e3f969e00f7
Avi Paz <pazavi@h-iMac-sl-Avi.local>
2020-12-16 18:06:12 +0200

    storage service

--- js/services/location-service.js
+++ js/services/location-service.js
@@ -1,11 +1,28 @@
+import { loadLocationsFromStorage } from '../services/storage-service.js'
+
+console.log('loadLocationsFromStorage',loadLocationsFromStorage);
 
 export const locationService = {
     getLocations
 }
 
+const STORAGE_KEY = 'myLocationsDB';
 
-const  gLocations = [{lat: 17, lng: 19, name: 'Puki Home'}];
+
+const  gLocations = getUserLocations();
+console.log ('gLocations:', gLocations);
+
+function getUserLocations(){
+    let userLocations = loadFromStorage(STORAGE_KEY);
+    if (!userLocations) userLocations = [{lat: 17, lng: 19, name: 'Puki Home'}];
+    return userLocations;
+}
+
 
 function getLocations() {
     return Promise.resolve(gLocations)
+}
+
+function _saveMyLocationsToStorage() {
+    saveToStorage(STORAGE_KEY, gLocations)
 }
---
+++ js/services/storage-service.js
@@ -0,0 +1,14 @@
+export const loadLocationsFromStorage = {
+    loadFromStorage
+}
+
+function loadFromStorage(key) {
+    var json = localStorage.getItem(key)
+    var value = JSON.parse(json)
+    return value;
+}
+
+function saveToStorage(key, value) {
+    var json = JSON.stringify(value);
+    localStorage.setItem(key, json)
+}
